036a7fae838a97b55a7816dc5ff5d1c94b0d1054
Oles Bober <olesbober@umail.ucsb.edu>
2019-11-14 18:15:17 -0800

    ob - wrote a test that checked for 'Earthquakes' and made it pass

--- src/test/java/hello/HomePageTest.java
+++ src/test/java/hello/HomePageTest.java
@@ -60,4 +60,12 @@ public class HomePageTest {
                 .andExpect(xpath("//title").exists())
                 .andExpect(xpath("//title").string("CS56 Spring Boot Practice App"));
     }
+
+    @Test
+    public void getHomePage_hasCorrectPage1Title() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").string("Earthquakes"));
+    }
 }

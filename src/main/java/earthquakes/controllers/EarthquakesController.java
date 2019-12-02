1af591ca36f9ec568f7d6a440d03a74356c31538
Oles Bober <olesbober@umail.ucsb.edu>
2019-12-02 14:33:12 -0800

    Locations results page now shows all locations with the user search name and their place_ids, latitude, longitude, name, and type

--- src/main/java/earthquakes/controllers/EarthquakesController.java
+++ src/main/java/earthquakes/controllers/EarthquakesController.java
@@ -2,16 +2,11 @@ package earthquakes.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import earthquakes.geojson.FeatureCollection;
-import java.util.Map;
-import java.util.HashMap;
-import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 import earthquakes.services.EarthquakeQueryService;
 import earthquakes.searches.EqSearch;
 
@@ -30,7 +25,6 @@ public class EarthquakesController {
     public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken, EqSearch eqSearch) {
 
         EarthquakeQueryService e = new EarthquakeQueryService();
-
         model.addAttribute("eqSearch", eqSearch);
         String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
         model.addAttribute("json", json);

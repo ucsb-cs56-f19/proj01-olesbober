399a68bb8827613ad2e97a25861a80d531ab7469
Oles Bober <olesbober@umail.ucsb.edu>
2019-12-03 12:56:10 -0800

    cleaned up imports and unused variables

--- src/main/java/earthquakes/controllers/EarthquakesController.java
+++ src/main/java/earthquakes/controllers/EarthquakesController.java
@@ -1,8 +1,6 @@
 package earthquakes.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +10,6 @@ import earthquakes.searches.EqSearch;
 
 @Controller
 public class EarthquakesController {
-
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping("/earthquakes/search")
     public String getEarthquakesSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken, EqSearch eqSearch) {
--- src/main/java/earthquakes/services/EarthquakeQueryService.java
+++ src/main/java/earthquakes/services/EarthquakeQueryService.java
@@ -3,14 +3,11 @@ package earthquakes.services;
 import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -37,8 +34,6 @@ public class EarthquakeQueryService {
         String retVal="";
         try {   
             ResponseEntity<String> re = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
-            MediaType contentType = re.getHeaders().getContentType();
-            HttpStatus statusCode = re.getStatusCode();
             retVal = re.getBody();
         } catch (HttpClientErrorException e) {
             retVal = "{\"error\": \"401: Unauthorized\"}";
